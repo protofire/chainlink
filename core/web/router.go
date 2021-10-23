96d02671551a661d02f3626efc04f2457348e530
Jordan Krage <jmank88@gmail.com>
2021-10-23 08:08:30 -0500

    inject loggers; eliminate InfoF and Debugf helpers

--- core/web/router.go
+++ core/web/router.go
@@ -39,14 +39,6 @@ import (
 	"github.com/smartcontractkit/chainlink/core/web/schema"
 )
 
-func init() {
-	gin.DebugPrintRouteFunc = printRoutes
-}
-
-func printRoutes(httpMethod, absolutePath, handlerName string, nuHandlers int) {
-	logger.Debugf("%-6s %-25s --> %s (%d handlers)", httpMethod, absolutePath, handlerName, nuHandlers)
-}
-
 const (
 	// SessionName is the session name
 	SessionName = "clsession"
