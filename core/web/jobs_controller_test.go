1f20f6f9699549cfd0f743b5b9158521021cce8a
koteld <festbot@gmail.com>
2021-10-30 02:46:51 +0300

    core/web go-ethereum -> celo-blockchain

--- core/web/jobs_controller_test.go
+++ core/web/jobs_controller_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/celo-org/celo-blockchain/common"
 	"github.com/smartcontractkit/chainlink/core/testdata/testspecs"
 
 	"github.com/pelletier/go-toml"
--- core/web/presenters/eth_tx.go
+++ core/web/presenters/eth_tx.go
@@ -3,8 +3,8 @@ package presenters
 import (
 	"strconv"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/celo-org/celo-blockchain/common"
+	"github.com/celo-org/celo-blockchain/common/hexutil"
 	"github.com/smartcontractkit/chainlink/core/services/bulletprooftxmanager"
 )
 
