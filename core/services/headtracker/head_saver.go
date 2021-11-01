dbbe10d32286d7e9cd03901ae92732686c55b135
koteld <festbot@gmail.com>
2021-11-01 18:52:47 +0300

    core/services/headtracker go-ethereum -> celo-blockchain

--- core/services/headtracker/head_saver.go
+++ core/services/headtracker/head_saver.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/celo-org/celo-blockchain/common"
 	"github.com/smartcontractkit/chainlink/core/services/postgres"
 	"github.com/smartcontractkit/chainlink/core/store/models"
 )
