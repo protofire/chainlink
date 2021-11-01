b255c4f30ee1173f1645c73ec1777579563d4dd5
koteld <festbot@gmail.com>
2021-11-01 15:54:11 +0300

    core/services/chainlink go-ethereum -> celo-blockchain

--- core/services/chainlink/application.go
+++ core/services/chainlink/application.go
@@ -12,8 +12,8 @@ import (
 	"sync"
 	"syscall"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/celo-org/celo-blockchain/common"
+	"github.com/celo-org/celo-blockchain/core/types"
 	"github.com/gobuffalo/packr"
 	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"
