031733a95882e4ef5354629724b1e80602dee031
koteld <festbot@gmail.com>
2021-11-02 12:47:17 +0300

    core/services/synchronization go-ethereum -> celo-blockchain

--- core/services/synchronization/telemetry_ingress_client.go
+++ core/services/synchronization/telemetry_ingress_client.go
@@ -7,7 +7,7 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/celo-org/celo-blockchain/common"
 	"github.com/smartcontractkit/chainlink/core/logger"
 	"github.com/smartcontractkit/chainlink/core/service"
 	"github.com/smartcontractkit/chainlink/core/services/keystore"
