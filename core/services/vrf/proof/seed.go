66c91bf96902a556c565d73e836cd83e0d0faa4c
koteld <festbot@gmail.com>
2021-11-02 13:01:32 +0300

    core/services/vrf go-ethereum -> celo-blockchain

--- core/services/vrf/proof/seed.go
+++ core/services/vrf/proof/seed.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/celo-org/celo-blockchain/common"
 	"github.com/pkg/errors"
 
 	"github.com/smartcontractkit/chainlink/core/utils"
