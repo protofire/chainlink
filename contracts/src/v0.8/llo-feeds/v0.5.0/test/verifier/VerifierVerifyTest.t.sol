b4610a6aba5155b6c9b17a3a960af460cd1df44d
Michael Fletcher <36506122+Fletch153@users.noreply.github.com>
2025-01-20 14:22:25 +0000

    V03 sbrv pc support impl 2 (#15338)
    * Base changes
    
    * Initial working impl of SBRV migration
    
    * Tests + fixes
    
    * Remaining test + fixes
    
    * Restore interface
    
    * Fix setConfig migration
    
    * Audit fixes
    
    * wrappers / abi
    
    * Prettier
    
    * Update gethwrappers
    
    * gas snapshot
    
    * Fixed dependency relocation and snapshot
    
    * Changeset
    
    * Fix gas snapshots
    
    * Remove script files wihch should not have been incl
    
    * Remove base gas snapshot
    
    ---------
    
    Co-authored-by: app-token-issuer-infra-releng[bot] <120227048+app-token-issuer-infra-releng[bot]@users.noreply.github.com>

--- contracts/src/v0.8/llo-feeds/v0.5.0/test/verifier/VerifierVerifyTest.t.sol
+++ contracts/src/v0.8/llo-feeds/v0.5.0/test/verifier/VerifierVerifyTest.t.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.19;
 
 import {BaseTestWithConfiguredVerifierAndFeeManager} from "./BaseVerifierTest.t.sol";
-import {Verifier} from "../../../v0.3.0/Verifier.sol";
-import {VerifierProxy} from "../../../v0.3.0/VerifierProxy.sol";
+import {Verifier} from "../../Verifier.sol";
+import {VerifierProxy} from "../../VerifierProxy.sol";
 import {AccessControllerInterface} from "../../../../shared/interfaces/AccessControllerInterface.sol";
 import {Common} from "../../../libraries/Common.sol";
 
@@ -16,8 +16,7 @@ contract VerifierVerifyTest is BaseTestWithConfiguredVerifierAndFeeManager {
 
   function setUp() public virtual override {
     BaseTestWithConfiguredVerifierAndFeeManager.setUp();
-    (, , bytes32 configDigest) = s_verifier.latestConfigDetails(FEED_ID);
-    s_reportContext[0] = configDigest;
+    s_reportContext[0] = v1ConfigDigest;
     s_reportContext[1] = bytes32(abi.encode(uint32(5), uint8(1)));
     s_testReportOne = _createV1Report(
       FEED_ID,
@@ -54,7 +53,7 @@ contract VerifierVerifyTest is BaseTestWithConfiguredVerifierAndFeeManager {
   }
 }
 
-contract VerifierProxyVerifyTest is VerifierVerifyTest {
+contract VerifierProxyVerifyTestV05 is VerifierVerifyTest {
   function test_revertsIfNoVerifierConfigured() public {
     s_reportContext[0] = bytes32("corrupt-digest");
     bytes memory signedReport = _generateV1EncodedBlob(
@@ -78,7 +77,7 @@ contract VerifierProxyVerifyTest is VerifierVerifyTest {
   }
 }
 
-contract VerifierProxyAccessControlledVerificationTest is VerifierVerifyTest {
+contract VerifierProxyAccessControlledVerificationTestV05 is VerifierVerifyTest {
   function setUp() public override {
     VerifierVerifyTest.setUp();
     AccessControllerInterface accessController = AccessControllerInterface(ACCESS_CONTROLLER_ADDRESS);
@@ -122,7 +121,7 @@ contract VerifierProxyAccessControlledVerificationTest is VerifierVerifyTest {
   }
 }
 
-contract VerifierVerifySingleConfigDigestTest is VerifierVerifyTest {
+contract VerifierVerifySingleConfigDigestTestV05 is VerifierVerifyTest {
   function test_revertsIfVerifiedByNonProxy() public {
     bytes memory signedReport = _generateV1EncodedBlob(
       s_testReportOne,
@@ -142,7 +141,7 @@ contract VerifierVerifySingleConfigDigestTest is VerifierVerifyTest {
     s_verifier.verify(signedReport, msg.sender);
   }
 
-  function test_revertsIfMismatchedSignatureLengthV03() public {
+  function test_revertsIfMismatchedSignatureLength() public {
     bytes32[] memory rs = new bytes32[](FAULT_TOLERANCE + 1);
     bytes32[] memory ss = new bytes32[](FAULT_TOLERANCE + 3);
     bytes32 rawVs = bytes32("");
@@ -160,12 +159,12 @@ contract VerifierVerifySingleConfigDigestTest is VerifierVerifyTest {
       reportContext,
       _getSigners(FAULT_TOLERANCE + 1)
     );
-    vm.expectRevert(abi.encodeWithSelector(Verifier.DigestInactive.selector, FEED_ID, reportContext[0]));
+    vm.expectRevert(abi.encodeWithSelector(Verifier.DigestInactive.selector, reportContext[0]));
     changePrank(address(s_verifierProxy));
     s_verifier.verify(signedReport, msg.sender);
   }
 
-  function test_revertsIfReportHasUnconfiguredFeedID() public {
+  function test_revertsIfReportHasUnconfiguredConfigDigest() public {
     V1Report memory report = _createV1Report(
       FEED_ID_2,
       OBSERVATIONS_TIMESTAMP,
@@ -177,8 +176,9 @@ contract VerifierVerifySingleConfigDigestTest is VerifierVerifyTest {
       BLOCKNUMBER_LOWER_BOUND,
       uint32(block.timestamp)
     );
+    s_reportContext[0] = keccak256("unconfigured-digesty");
     bytes memory signedReport = _generateV1EncodedBlob(report, s_reportContext, _getSigners(FAULT_TOLERANCE + 1));
-    vm.expectRevert(abi.encodeWithSelector(Verifier.DigestInactive.selector, FEED_ID_2, s_reportContext[0]));
+    vm.expectRevert(abi.encodeWithSelector(Verifier.DigestInactive.selector, s_reportContext[0]));
     changePrank(address(s_verifierProxy));
     s_verifier.verify(signedReport, msg.sender);
   }
@@ -212,20 +212,6 @@ contract VerifierVerifySingleConfigDigestTest is VerifierVerifyTest {
     assertReportsEqual(response, s_testReportOne);
   }
 
-  function test_setsTheCorrectEpoch() public {
-    s_reportContext[1] = bytes32(uint256(5 << 8));
-    bytes memory signedReport = _generateV1EncodedBlob(
-      s_testReportOne,
-      s_reportContext,
-      _getSigners(FAULT_TOLERANCE + 1)
-    );
-    changePrank(address(s_verifierProxy));
-    s_verifier.verify(signedReport, msg.sender);
-
-    (, , uint32 latestEpoch) = s_verifier.latestConfigDigestAndEpoch(FEED_ID);
-    assertEq(latestEpoch, 5);
-  }
-
   function test_emitsAnEventIfReportVerified() public {
     bytes memory signedReport = _generateV1EncodedBlob(
       s_testReportOne,
@@ -239,7 +225,7 @@ contract VerifierVerifySingleConfigDigestTest is VerifierVerifyTest {
   }
 }
 
-contract VerifierVerifyMultipleConfigDigestTest is VerifierVerifyTest {
+contract VerifierVerifyMultipleConfigDigestTestV05 is VerifierVerifyTest {
   bytes32 internal s_oldConfigDigest;
   bytes32 internal s_newConfigDigest;
 
@@ -247,22 +233,31 @@ contract VerifierVerifyMultipleConfigDigestTest is VerifierVerifyTest {
 
   function setUp() public override {
     VerifierVerifyTest.setUp();
-    (, , s_oldConfigDigest) = s_verifier.latestConfigDetails(FEED_ID);
-    s_verifier.setConfig(
+    s_oldConfigDigest = v1ConfigDigest;
+
+    s_newConfigDigest = _configDigestFromConfigData(
       FEED_ID,
+      SOURCE_CHAIN_ID,
+      SOURCE_ADDRESS,
+      2,
       _getSignerAddresses(_getSigners(20)),
       s_offchaintransmitters,
       FAULT_TOLERANCE_TWO,
       bytes(""),
       VERIFIER_VERSION,
-      bytes(""),
+      bytes("")
+    );
+
+    s_verifier.setConfig(
+      s_newConfigDigest,
+      _getSignerAddresses(_getSigners(20)),
+      FAULT_TOLERANCE_TWO,
       new Common.AddressAndWeight[](0)
     );
-    (, , s_newConfigDigest) = s_verifier.latestConfigDetails(FEED_ID);
   }
 
   function test_revertsIfVerifyingWithAnUnsetDigest() public {
-    s_verifier.deactivateConfig(FEED_ID, (s_oldConfigDigest));
+    s_verifier.deactivateConfig(s_oldConfigDigest);
 
     bytes memory signedReport = _generateV1EncodedBlob(
       s_testReportOne,
@@ -270,7 +265,7 @@ contract VerifierVerifyMultipleConfigDigestTest is VerifierVerifyTest {
       _getSigners(FAULT_TOLERANCE + 1)
     );
     changePrank(address(s_verifierProxy));
-    vm.expectRevert(abi.encodeWithSelector(Verifier.DigestInactive.selector, FEED_ID, s_reportContext[0]));
+    vm.expectRevert(abi.encodeWithSelector(Verifier.DigestInactive.selector, s_reportContext[0]));
     s_verifier.verify(signedReport, msg.sender);
   }
 
@@ -310,5 +305,60 @@ contract VerifierVerifyMultipleConfigDigestTest is VerifierVerifyTest {
     );
     changePrank(address(s_verifierProxy));
     s_verifier.verify(signedReport, msg.sender);
+  }
+
+  function test_verifyAfterConfigUpdate() public {
+    // Get initial signers and set initial config
+    address[] memory initialSigners = _getSignerAddresses(_getSigners(15));
+    bytes32 configDigest = keccak256("test");
+
+    s_verifier.setConfig(configDigest, initialSigners, 4, new Common.AddressAndWeight[](0));
+
+    // Get new signers and update config
+    address[] memory newSigners = _getSignerAddresses(_getSigners(20));
+    s_verifier.updateConfig(configDigest, initialSigners, newSigners, 6);
+
+    // Verify report with new signers should pass
+    s_reportContext[0] = configDigest;
+    bytes memory signedReportNewSigners = _generateV1EncodedBlob(
+      s_testReportOne,
+      s_reportContext,
+      _getSigners(7) // More than f=6 signers
+    );
+
+    bytes memory response = s_verifierProxy.verify(signedReportNewSigners, bytes(""));
+    assertReportsEqual(response, s_testReportOne);
+
+    // Verify report with old signers should fail
+    bytes memory signedReportOldSigners = _generateV1EncodedBlob(
+      s_testReportOne,
+      s_reportContext,
+      _getSigners(5) // Old number of signers
+    );
+    vm.expectRevert(abi.encodeWithSelector(Verifier.IncorrectSignatureCount.selector, 5, 7));
+
+    s_verifierProxy.verify(signedReportOldSigners, bytes(""));
+  }
+
+  function test_verifyAfterConfigUpdateWithExistingSigners() public {
+    // Get initial signers and set initial config
+    address[] memory signers = _getSignerAddresses(_getSigners(15));
+    bytes32 configDigest = keccak256("test");
+
+    s_verifier.setConfig(configDigest, signers, 4, new Common.AddressAndWeight[](0));
+
+    // Update config with same signers and f
+    s_verifier.updateConfig(configDigest, signers, signers, 4);
+
+    // Verify report should pass
+    s_reportContext[0] = configDigest;
+    bytes memory signedReport = _generateV1EncodedBlob(
+      s_testReportOne,
+      s_reportContext,
+      _getSigners(5) // More than f=4 signers
+    );
+
+    bytes memory response = s_verifierProxy.verify(signedReport, bytes(""));
+    assertReportsEqual(response, s_testReportOne);
   }
 }
